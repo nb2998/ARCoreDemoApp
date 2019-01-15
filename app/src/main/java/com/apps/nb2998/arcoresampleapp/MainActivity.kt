da5307dd156bdca29560068b37bf8a7233fc92af
Nishtha Bhatia <nbhatia329@gmail.com>
2019-01-15 19:44:21 +0530

    Plane detection using ARFragment

--- app/src/main/java/com/apps/nb2998/arcoresampleapp/MainActivity.kt
+++ app/src/main/java/com/apps/nb2998/arcoresampleapp/MainActivity.kt
@@ -2,11 +2,28 @@ package com.apps.nb2998.arcoresampleapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.ar.sceneform.ux.ArFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var arFragment: ArFragment
+    private var isTracking = false
+    private var isHitting = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        arFragment = sceneform_frag as ArFragment
+        arFragment = supportFragmentManager.findFragmentById(R.id.sceneform_frag) as ArFragment
+        arFragment.arSceneView.scene.addOnUpdateListener {
+            arFragment.onUpdate(it)
+            onUpdate()
+        }
+    }
+
+    private fun onUpdate() {
+
     }
 }
